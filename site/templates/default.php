938aa53781627ff02f87c3611022373c706a543d
Alisa <alisa.blakeney@gmail.com>
2020-08-13 20:37:08 +1000

    tried to do something with styles. probably wind this back tomorrow

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,4 +2,21 @@
 
     <h1><?= $page->title() ?></h1>
 
+
+    <ul>
+        <? foreach(page('contributors')->children() as $contributor): ?>
+            <li><a href="<?= $contributor->url() ?>"><?= $contributor->title() ?> </a> </li>
+        <? endforeach ?>
+    </ul>
+
+    <ul>
+        <? foreach(page('works-texts')->children() as $single): ?>
+            <li><a href="<?= $single->url() ?>"><?= $single->title() ?> </a> </li>
+        <? endforeach ?>
+    </ul>
+
+    <? $about= page('about') ?>
+    <? $about->title() ?>
+    <? $about->text()->kirbytext() ?>
+
 <? snippet('footer') ?>
